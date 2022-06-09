8b47c667e142bec02dc1892ff8f2152e1ec90de0
aryannaf <aryannafarhadi@gmail.com>
2022-06-08 21:27:22 -0400

    Served images as static assets from server

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
+
+const videoRoutes = require('./routes/videos');
+app.use('/videos', videoRoutes);
+app.use('/videos/:id', videoRoutes);
+
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 })
